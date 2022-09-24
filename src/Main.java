4fae490246fe7778f5b3ec7b0f99be5b94be58cd
Murat1ck <muratoff101088@gmail.com>
2022-09-24 12:43:55 +0300

    HW3.5

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,17 @@
 public class Main {
     public static void main(String[] args) {
 
-        int i = 10000;
-        byte b = 12;
-        short s = 123;
-        long l = 234l;
-        float f = 2.17f;
-        double d = 4.19;
+        int totalPaint = 120;
+        int whitePaint = 2;
+        int brownPaint = 4;
+        int oneClass = whitePaint + brownPaint;
+        int totalClasses = totalPaint / oneClass;
+        int totalWhite = totalClasses * whitePaint;
+        int totalBrown = totalClasses * brownPaint;
+        System.out.println("В школе, где " + totalClasses + " классов, нужно " + totalWhite + " банок белой краски и " + totalBrown + " банок коричневой краски");
 
-        System.out.println("Значение переменной i с типом int равно: " + i);
-        System.out.println("Значение переменной b с типом byte равно: " + b);
-        System.out.println("Значение переменной s с типом short равно: " + s);
-        System.out.println("Значение переменной l с типом long равно: " + l);
-        System.out.println("Значение переменной f с типом float равно: " + f);
-        System.out.println("Значение переменной d с типом double равно: " + d);
+
+
+
     }
 }
