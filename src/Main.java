ab0ca60aef4dcb259e49f1e66645af2b1ab4d943
Murat1ck <muratoff101088@gmail.com>
2022-09-24 11:31:03 +0300

    HW3.1

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int i = 10000;
+        byte b = 12;
+        short s = 123;
+        long l = 234l;
+        float f = 2.17f;
+        double d = 4.19;
+
+        System.out.println("Значение переменной i с типом int равно: " + i);
+        System.out.println("Значение переменной b с типом byte равно: " + b);
+        System.out.println("Значение переменной s с типом short равно: " + s);
+        System.out.println("Значение переменной l с типом long равно: " + l);
+        System.out.println("Значение переменной f с типом float равно: " + f);
+        System.out.println("Значение переменной d с типом double равно: " + d);
+    }
+}
