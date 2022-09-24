011ef2cc56c58383ed3d392ce92767188c43a607
Murat1ck <muratoff101088@gmail.com>
2022-09-24 14:03:29 +0300

    HW3.8

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,36 @@
 public class Main {
     public static void main(String[] args) {
 
-        int i = 10000;
-        byte b = 12;
-        short s = 123;
-        long l = 234l;
-        float f = 2.17f;
-        double d = 4.19;
+        float salaryMasha = 67_760;
+        float salaryDenis = 83_690;
+        float salaryKristina = 76_230;
 
-        System.out.println("Значение переменной i с типом int равно: " + i);
-        System.out.println("Значение переменной b с типом byte равно: " + b);
-        System.out.println("Значение переменной s с типом short равно: " + s);
-        System.out.println("Значение переменной l с типом long равно: " + l);
-        System.out.println("Значение переменной f с типом float равно: " + f);
-        System.out.println("Значение переменной d с типом double равно: " + d);
+        double raisingSalaryMasha = 67_760 * 0.1; //сумма увеличения
+        double raisingSalaryDenis = 83_690 * 0.1;
+        double raisingSalaryKristina = 76_230 * 0.1;
+
+        double increasedSalaryMasha  = salaryMasha + raisingSalaryMasha; //увеличенная зп
+        double increasedSalaryDenis  = salaryDenis + raisingSalaryDenis;
+        double increasedSalaryKristina  = salaryKristina + raisingSalaryKristina;
+        int a = 12; //год
+        float annualSalaryMasha = salaryMasha * a; //зп за год
+        float annualSalaryDenis = salaryDenis * a;
+        float annualSalaryKristina = salaryKristina * a;
+
+        double annualIncreasedSalaryMasha  = increasedSalaryMasha * a;  //увеличенная зп за год
+        double annualIncreasedSalaryDenis  = increasedSalaryDenis * a;
+        double annualIncreasedSalaryKristina  = increasedSalaryKristina * a;
+
+        double b = annualIncreasedSalaryMasha - annualSalaryMasha; // разница дохода за год
+        double c = annualIncreasedSalaryDenis - annualSalaryDenis;
+        double d = annualIncreasedSalaryKristina - annualSalaryKristina;
+
+        System.out.println( "Маша теперь получает " + increasedSalaryMasha + " рублей. Годовой доход вырос на: " + b + " рублей");
+        System.out.println( "Денис теперь получает " + increasedSalaryDenis + " рублей. Годовой доход вырос на: " + c + " рублей");
+        System.out.println( "Кристина теперь получает " + increasedSalaryKristina + " рублей. Годовой доход вырос на: " + d + " рублей");
+
+        System.out.println(increasedSalaryKristina);
+
+
     }
 }
